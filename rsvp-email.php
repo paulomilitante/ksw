e8ed7f11c7e2d619feff8814c5d94a689034f2f4
Paulo Militante <p.militante@creativehope.jp>
2018-08-29 15:47:34 +0800

    Edit sendgrid

--- rsvp-email.php
+++ rsvp-email.php
@@ -18,17 +18,27 @@ $body = "<html>
 </body>
 </html>";
 
-$from = new SendGrid\Email(null,"kokoandsani2018@gmail.com");
+// $from = new SendGrid\Email(null,"kokoandsani2018@gmail.com");
 $subject = "RSVP - $name";
-$to = new SendGrid\Email(null, "kokoandsani2018@gmail.com");
-$content = new SendGrid\Content("text/html", $body);
-$mail = new SendGrid\Mail($from, $subject, $to, $content);
+// $to = new SendGrid\Email(null, "kokoandsani2018@gmail.com");
+// $content = new SendGrid\Content("text/html", $body);
+// $mail = new SendGrid\Mail($from, $subject, $to, $content);
 
-$apiKey = getenv('SENDGRID_API_KEY');
-$sg = new \SendGrid($apiKey);
+// $apiKey = getenv('SENDGRID_API_KEY');
+// $sg = new \SendGrid($apiKey);
 
-$response = $sg->client->mail()->send()->post($mail);
-echo $response->body();
+// $response = $sg->client->mail()->send()->post($mail);
+// echo $response->body();
 // include("success.html");
 
+
+$sendgrid = new SendGrid("SENDGRID_API_KEY");
+$email    = new SendGrid\Email();
+
+$email->addTo("kokoandsani2018@gmail.com")
+      ->setFrom("kokoandsani2018@gmail.com")
+      ->setSubject($subject)
+      ->setHtml($body);
+
+$sendgrid->send($email);
 ?>
