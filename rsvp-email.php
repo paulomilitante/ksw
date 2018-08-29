1533f643d5f0409ccaf1d174ce48543b9fd04dcf
Paulo Militante <p.militante@creativehope.jp>
2018-08-29 16:01:42 +0800

    Edit sendgrid

--- rsvp-email.php
+++ rsvp-email.php
@@ -2,7 +2,7 @@
 
 require "sendgrid-php/sendgrid-php.php";
 
-$email = $_POST['email'];
+$uemail = $_POST['email'];
 $name = $_POST['name'];
 $mobile = $_POST['mobile-number'];
 $attend = $_POST['attend'];
@@ -12,33 +12,30 @@ $body = "<html>
 <body>
 <p><b>Full Name:</b> $name</p>
 <p><b>Mobile:</b> $mobile</p>
-<p><b>Email:</b> $email</p>
+<p><b>Email:</b> $uemail</p>
 <p><b>Response:</b> $attend</p>
 <p><b>Message:</b> $message</p>
 </body>
 </html>";
 
-// $from = new SendGrid\Email(null,"kokoandsani2018@gmail.com");
 $subject = "RSVP - $name";
-// $to = new SendGrid\Email(null, "kokoandsani2018@gmail.com");
-// $content = new SendGrid\Content("text/html", $body);
-// $mail = new SendGrid\Mail($from, $subject, $to, $content);
 
 $apiKey = getenv('SENDGRID_API_KEY');
-// $sg = new \SendGrid($apiKey);
-
-// $response = $sg->client->mail()->send()->post($mail);
-// echo $response->body();
-// include("success.html");
 
 
-$sendgrid = new SendGrid($apiKey);
-$email    = new SendGrid\Email();
+$email = new \SendGrid\Mail\Mail(); 
+$email->setFrom("kokoandsani2018@gmail.com", "");
+$email->setSubject($subject);
+$email->addTo("kokoandsani2018@gmail.com", "");
+$email->addContent("text/html", $body);
 
-$email->addTo("kokoandsani2018@gmail.com")
-      ->setFrom("kokoandsani2018@gmail.com")
-      ->setSubject($subject)
-      ->setHtml($body);
-
-$sendgrid->send($email);
+$sendgrid = new \SendGrid(getenv('SENDGRID_API_KEY'));
+try {
+    $response = $sendgrid->send($email);
+    print $response->statusCode() . "\n";
+    print_r($response->headers());
+    print $response->body() . "\n";
+} catch (Exception $e) {
+    echo 'Caught exception: '. $e->getMessage() ."\n";
+}
 ?>
